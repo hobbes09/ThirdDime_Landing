83f08dd587da396bc86c195a72512f01ddf74303
Rohit <rgarg1992@gmail.com>
2014-12-15 21:13:47 +0530

    Adding percentage to Question 3 and 7 funda page.

--- thirdDime/static/js/funda/funda.controller.js
+++ thirdDime/static/js/funda/funda.controller.js
@@ -14,18 +14,18 @@ angular.module("Funda", ['ngMaterial', "Data"])
 .controller("DaliController", ['$scope', "data", "$timeout", function ($scope, data, $timeout) {
 	data.getVotes(1).then(function (data) {
 		var sum = 100/(data['one'] + data['two'] + data['three'] +data['four']);
-		$scope.vote31 = data['one']*sum;
-		$scope.vote32 = data['two']*sum;
-		$scope.vote33 = data['three']*sum;
-		$scope.vote34 = data['four']*sum;
+		$scope.vote31 = Math.round(data['one']*sum);
+		$scope.vote32 = Math.round(data['two']*sum);
+		$scope.vote33 = Math.round(data['three']*sum);
+		$scope.vote34 = Math.round(data['four']*sum);
 	});
 }])
 .controller("ArmidaController", ['$scope', "data", function ($scope, data) {
 	data.getVotes(2).then(function (data) {
 		var sum = 100/(data['one'] + data['two'] + data['three'] +data['four']);
-		$scope.vote31 = data['one']*sum;
-		$scope.vote32 = data['two']*sum;
-		$scope.vote33 = data['three']*sum;
-		$scope.vote34 = data['four']*sum;
+		$scope.vote71 = Math.round(data['one']*sum);
+		$scope.vote72 = Math.round(data['two']*sum);
+		$scope.vote73 = Math.round(data['three']*sum);
+		$scope.vote74 = Math.round(data['four']*sum);
 	})
 }]);
