f41fdc703e3ca35a84a19b1e4bb2ccb8cc37e10d
Rohit <rgarg1992@gmail.com>
2014-12-30 04:06:22 +0530

    Fix bugs introduced during grunt implementation

--- thirdDime/Gruntfile.js
+++ thirdDime/Gruntfile.js
@@ -4,9 +4,19 @@ module.exports = function (grunt) {
 
 		clean: [ 'static/build', '.tmp'],
 		copy: {
-			build: {
-				src: 'templates/index.html',
-				dest: '.tmp/index.html'
+			build:{
+				files: [
+					{
+						expand: true,
+						cwd: './static/build/static/build',
+						src: '*',
+						dest: './static/build'
+					},
+					{
+						src: 'templates/index.html',
+						dest: '.tmp/index.html'
+					},
+				],
 			},
 			cleanBuild: {
 				src: '.tmp/index.html',
@@ -26,13 +36,7 @@ module.exports = function (grunt) {
 		},
 	});
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-filerev');
-    grunt.loadNpmTasks('grunt-usemin');
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.registerTask('build', [
 	  'useminPrepare',
--- thirdDime/static/js/thirdDime.module.js
+++ thirdDime/static/js/thirdDime.module.js
@@ -1,5 +1,5 @@
 angular.module("ThirdDime", ["Quiz", "ngSocial", "Data", "ngAnimate"])
-.config(function ($httpProvider, $interpolateProvider) {
+.config(['$httpProvider', '$interpolateProvider', function ($httpProvider, $interpolateProvider) {
   // $interpolateProvider.startSymbol('{[{');
   // $interpolateProvider.endSymbol('}]}');
 
@@ -7,4 +7,4 @@ angular.module("ThirdDime", ["Quiz", "ngSocial", "Data", "ngAnimate"])
     // $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-});
+}]);
