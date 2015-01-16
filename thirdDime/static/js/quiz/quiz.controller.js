7b0077d68e2d179cc06924820d11040a6fe0eb79
Rohit <rgarg1992@gmail.com>
2015-01-16 10:06:44 +0530

    Changing sequence of questions.

--- thirdDime/static/js/quiz/quiz.controller.js
+++ thirdDime/static/js/quiz/quiz.controller.js
@@ -3,15 +3,15 @@ angular.module("Quiz", ["ngMaterial", "Intro", "Que", "Funda"])
 	"use strict";
 
 	$scope.quizes = [
-		{"color": "#3F51B5", "intro": "In Death, the union of heaven and earth", "flipped": false},
-		{"color": "#825682", "intro": "That the Immaculate Mother of God, the ever Virgin Mary, having completed the course of her earthly life, was borne by angels into heaven", "flipped": false},
-		{"color": "#296666", "intro": "In dreams we wake up to the starkest realities", "flipped": false},
-		{"color": "#6C6C50", "intro": "Marie Antoinette was the last Queen of France. During her reign her popularity amongst her subjects saw a downward curve. The painting that you are looking for can very well be called her last attempt at improving her public image.", "flipped": false},
-		{"color": "#000A00", "intro": "Canvas is a two dimensional space, but very often a painter has to step out of such bounds to achieve the unachievable.", "flipped": false},
 		{"color": "#8A5C2E", "intro": "What Light is to Impressionist; Color is to Fauv; What Space is to cubist; Motion is to Futurist", "flipped": false},
+		{"color": "#296666", "intro": "In dreams we wake up to the starkest realities", "flipped": false},
+		{"color": "#000A00", "intro": "Canvas is a two dimensional space, but very often a painter has to step out of such bounds to achieve the unachievable.", "flipped": false},
+		{"color": "#3F51B5", "intro": "In Death, the union of heaven and earth", "flipped": false},
 		{"color": "#2F4166", "intro": "The story of Armida, a Saracen sorceress and Rinaldo, a soldier in the First Crusade, was created by the Italian poet Torquato Tasso. Over time a large number of painters have brought this story to canvas. Rinaldo is a fierce, determined warrior. Armida has been sent to stop the Christians from completing their mission and is about to murder the sleeping soldier, but instead falls in love with him at first sight. She is now conflicted and confused by her changing emotions. ", "flipped": false},
+		{"color": "#825682", "intro": "That the Immaculate Mother of God, the ever Virgin Mary, having completed the course of her earthly life, was borne by angels into heaven", "flipped": false},
 		{"color": "#478D5E", "intro": "It starts with a simple desire to replicate all that is explicit", "flipped": false},
 		{"color": "#A34747", "intro": "War does not determine who is right, only who is left", "flipped": false},
+		{"color": "#6C6C50", "intro": "Marie Antoinette was the last Queen of France. During her reign her popularity amongst her subjects saw a downward curve. The painting that you are looking for can very well be called her last attempt at improving her public image.", "flipped": false},
 		{"color": "#A34747", "intro": "Relax for a couple of minutes, enjoy the painting that is to follow. Then click on the painting and let your thoughts transcend into words.", "flipped": false},
 	];
 
